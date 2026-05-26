5dfc079d506a87f8977f30f1ae191c96ff705264
tomoIss <iss6.morisako.tomoya@gmail.com>
2026-05-26 16:29:16 +0900

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -141,7 +141,6 @@ async function init() {
     if (userName) {
         showClassSelection(false);
         document.getElementById('username-init-modal').style.display = 'flex';
-        return; 
     }
     // クラスの選択情報があるかチェック
     if (!currentClass) {
@@ -174,7 +173,7 @@ function submitInitialUsername() {
 
     document.getElementById('username-init-modal').style.display = 'none';
 
-    init();
+    // init();
 }
 
 // クラスリストのみを取得して変数に格納する内部関数
